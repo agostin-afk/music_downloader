2c56a93ada9cb8a13f1b1ccc678c80a229016ca3
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-10-22 14:49:51 -0300

    __init__

---
+++ scripts/baixar_playlist_completa.py
@@ -0,0 +1,40 @@
+from pytube import Playlist, YouTube
+import threading
+import os
+
+p = Playlist("https://www.youtube.com/playlist?list=PL-EWx9Mw_fia6Od7xe3w3EdYrIdEn_DQm")
+
+print(f'A playlist escolhida foi: {p.title.encode("utf-8").decode("utf-8")}')
+
+videos_erros = []
+
+def download_audio(video_url):
+    try:
+        yt = YouTube(video_url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        audio_file = audio_stream.download()  # type: ignore
+        base, ext = os.path.splitext(audio_file)
+        new_file = base + '.mp3'
+        os.rename(audio_file, new_file)
+        print(f'Áudio do vídeo {yt.title.encode("utf-8").decode("utf-8")} baixado e convertido para mp3 com sucesso.')
+    except Exception as e:
+        videos_erros.append(video_url)
+        print(f'Erro ao baixar o áudio do vídeo {video_url}: {e}')
+
+threads = []
+
+for i in p.video_urls:
+    t = threading.Thread(target=download_audio, args=(i,))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+if videos_erros:
+    print(f'Os seguintes vídeos tiveram erros durante o download: {videos_erros}')
+else:
+    print('Todos os vídeos foram baixados com sucesso.')
+
+for _ in videos_erros:
+    download_audio(_)
---
+++ scripts/testespytube.py
@@ -0,0 +1,7 @@
+# from pytube import Playlist, YouTube
+# teste_playlist = Playlist("https://www.youtube.com/playlist?list=PL-EWx9Mw_fia6Od7xe3w3EdYrIdEn_DQm")
+
+# for _ in teste_playlist:
+#     print(_)
+#     print()
+
