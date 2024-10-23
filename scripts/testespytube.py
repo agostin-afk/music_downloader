56835b060d263b8dc9ce22b95ff3c495ab3048cb
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-10-23 16:10:02 -0300

    Update testespytube.py

--- scripts/testespytube.py
+++ scripts/testespytube.py
@@ -1,7 +1,9 @@
-# from pytube import Playlist, YouTube
-# teste_playlist = Playlist("https://www.youtube.com/playlist?list=PL-EWx9Mw_fia6Od7xe3w3EdYrIdEn_DQm")
+from pytube import Playlist, YouTube
+teste_playlist = Playlist("https://www.youtube.com/playlist?list=PL-EWx9Mw_fia6Od7xe3w3EdYrIdEn_DQm")
 
-# for _ in teste_playlist:
-#     print(_)
-#     print()
+for _ in teste_playlist.videos:
+    try:
+        print(_.title)
+    except KeyError:
+        print(f"Erro ao obter título do vídeo: {_.watch_url}")
 
