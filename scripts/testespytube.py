665df6a13609c8fd9f76159c03096439897f24e5
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-10-25 13:16:10 -0300

    Update testespytube.py

--- scripts/testespytube.py
+++ scripts/testespytube.py
@@ -1,12 +1,45 @@
 from pytube import Playlist, YouTube
-teste_playlist = Playlist("https://www.youtube.com/watch?v=Vi6bjV876fY&list=PLc0FtbJjhNUyOVaQ5r0sqd9YewHM9PSoL")
+import os
+import datetime
+
+
+def create_Arqv(name="list_erro_videos"):
+    try:
+        name = datetime.date.today()
+    except Exception as e:
+        print(f"Erro: {e}")
+        pass
+    name = str(name)+".txt"
+    try:
+        with open(name, 'x', encoding="UTF-8") as arqv:
+            pass
+    except FileExistsError:
+        print("Arqv já existe, apagando e trocando os valores")
+        os.remove(name)
+        create_Arqv(name)
+    except Exception as e:
+        print(f"Erro: {e}")
+        return None
+    return name
+
+def add_Video_Erro_List(name_video, name_arqv):
+    try:
+        with open(name_arqv, 'a') as file:
+            file.write(name_video+"\n")
+    except:
+        print("Erro")
+
+teste_playlist = Playlist("https://www.youtube.com/playlist?list=PLjpaTyNnuw2CVu4D1-OjK7KvocgRmDjDj")
 def teste_Criar_arqv(url):
-    for _ in url.videos:
+    name_arqv = create_Arqv()
+    for _ in url:
         try:
             # print(_.watch_url)
-            print(_.title)
+            print(_)
         except KeyError:
+            add_Video_Erro_List(f"{_}", name_arqv)
             print(f"Erro ao obter título do vídeo: {_}")
+            
 
 teste_Criar_arqv(teste_playlist)
 # lembrar de criar um jeito de tentar baixar 3 vezes e depois criar um arquivo com o nome e/ou a url
