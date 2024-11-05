5db94ceb7597203f1c9e5bbe7f16db3d8a94f8b8
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-11-05 18:08:28 -0300

    text return in widget

--- App/tkinterdesing/generate/build/Scripts/downloader.py
+++ App/tkinterdesing/generate/build/Scripts/downloader.py
@@ -1,23 +1,23 @@
 from pytubefix import YouTube, Playlist
-from testespytube import *
+# from .testespytube import *
 import threading
 import os
 import datetime
 
 class Audio:
     def __init__(self):
-        self.playlist_url = Playlist(playlist_url)
+        self.video_url = str
         self.name_arqv = self.create_Arqv()
-    def download_audio(self):
-        for video_url in self.playlist_url:
+    def download_audio(self, video_url):
             try:
                 yt = YouTube(video_url)
                 audio_stream = yt.streams.filter(only_audio=True).first()
-                audio_file = audio_stream.download(mp3=True)  # type: ignore
-                print(f'Áudio do vídeo {yt.title.encode("utf-8").decode("utf-8")} baixado e convertido para mp3 com sucesso.')
+                #audio_file = audio_stream.download(mp3=True)  # type: ignore
+                self.create_Arqv()
+                return f'Áudio do vídeo {yt.title.encode("utf-8").decode("utf-8")} baixado e convertido para mp3 com sucesso.'
             except Exception as e:
-                print(f'Erro ao baixar o áudio do vídeo {video_url}: {e}')
-                add_Arquivo_erro(video_url, self.name_arqv)    
+                # add_Arquivo_erro(video_url, self.name_arqv)    
+                return f'Erro ao baixar o áudio do vídeo {video_url}: {e}'
 
     def create_Arqv(self, name="list_erro_videos"):
         try:
@@ -30,18 +30,18 @@ class Audio:
             with open(name, 'x', encoding="UTF-8") as arqv:
                 pass
         except FileExistsError:
-            print("Arqv já existe, apagando e trocando os valores")
             os.remove(name)
             self.create_Arqv(name)
+            return "Arqv já existe, apagando e trocando os valores"
         except Exception as e:
-            print(f"Erro: {e}")
-            return None
-        return name
+            return f"Erro: {e}"
+        return str(name)
     def add_Video_Erro_List(self, name_video, name_arqv):
         try:
             with open(name_arqv, 'a') as file:
                 file.write(name_video+"\n")
         except:
-            print("Erro")
+            return "Erro"
     def add_Arquivo_erro(self,url, name_arqv):
-        add_Video_Erro_List(f"{url}", name_arqv)
+        # add_Video_Erro_List(f"{url}", name_arqv)
+        pass
--- App/tkinterdesing/generate/build/gui.py
+++ App/tkinterdesing/generate/build/gui.py
@@ -2,183 +2,98 @@ from dotenv import load_dotenv
 from pathlib import Path
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 import os
-from Scripts.downloader import download_audio
+from Scripts.downloader import Audio
 import time
 
+from pytubefix import Playlist
+
+# Diretório base e carregamento de variáveis de ambiente
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
 load_dotenv(BASE_DIR / 'dotenv_files' / '.env', override=True)
 OUTPUT_PATH = os.getenv('DIR_FILE')
 ASSETS_PATH = str(OUTPUT_PATH) + "\\assets\\frame0"
 
-from pytube import Playlist, YouTube
-current_message_index = 0
-messages_to_display = []
-
-
-def update_terminal_output(new_message):
-    current_text = canvas.itemcget(terminal_text_id, "text")
-    messages = current_text.split("\n")
-
-    messages.append(new_message)
-
-    max_lines = 10
-    if len(messages) > max_lines:
-        messages.pop(0)
-
-    updated_text = "\n".join(messages)
-    canvas.itemconfig(terminal_text_id, text=updated_text)
-
-
-def display_messages():
-    global current_message_index
-
-    if current_message_index < len(messages_to_display):
-        update_terminal_output(messages_to_display[current_message_index])
-        current_message_index += 1
-        window.after(500, display_messages) 
-
-
-def download():
-    global current_message_index, messages_to_display
-
-    nome_playlist_get = entry_1.get()
-    p = Playlist(f"{nome_playlist_get}")
-    nome_playlist = p.title.encode("utf-8").decode("utf-8")
-    canvas.itemconfig(text_id, text=nome_playlist)
-    messages_to_display = [f"Título da playlist: {nome_playlist}" for _ in range(20)]
-    current_message_index = 0
-    display_messages()
-
-
-
+# Função auxiliar para obter o caminho dos assets
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
+# Função de download que exibe o título da playlist e mensagens de progresso
+def download():
+    p = Audio()
+    playlist_url = entry_1.get()
+    playlist = Playlist(playlist_url)
+    
+    # # Atualiza o título da playlist
+    title_box = playlist.title.encode("utf-8").decode("utf-8")
+    canvas.itemconfig(text_id, text=title_box)
 
+    # Exibe mensagem de progresso no Text widget
+    for _ in playlist:
+        text_widget.config(state="normal")
+        text_widget.insert("end",f"{p.download_audio(_)}\n")
+        text_widget.config(state="disabled")
+
+# Configurações principais da janela e Canvas
 window = Tk()
-
 window.geometry("670x450")
-window.configure(bg = "#FFFFFF")
-
+window.configure(bg="#FFFFFF")
 
 canvas = Canvas(
     window,
-    bg = "#FFFFFF",
-    height = 450,
-    width = 670,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-
-canvas.place(x = 0, y = 0)
-"""background direita"""
-canvas.create_rectangle(
-    0.0,
-    0.0,
-    670.0,
-    450.0,
-    fill="#D9D9D9",
-    outline="")
-
-"""background esquerda"""
-canvas.create_rectangle(
-    0.0,
-    0.0,
-    261.0,
-    450.0,
-    fill="#717171",
-    outline="")
-
-"""Youtube icon"""
-image_image_1 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_1 = canvas.create_image(
-    130.0,
-    117.0,
-    image=image_image_1
-)
-
-"""text abaixo do youtube icon"""
-canvas.create_text(
-    127.0,
-    230.0,
-    text="Downloader",
-    fill="#000000",
-    font=("Inter", 21 * -1)
-)
-
-entry_image_1 = PhotoImage(
-    file=relative_to_assets("entry_1.png"))
-entry_bg_1 = canvas.create_image(
-    130.5,
-    320.5,
-    image=entry_image_1
-)
-entry_1 = Entry(
+    bg="#FFFFFF",
+    height=450,
+    width=670,
     bd=0,
-    bg="#D9D9D9",
-    fg="#000716",
+    highlightthickness=0,
+    relief="ridge"
+)
+canvas.place(x=0, y=0)
+
+# Desenho dos elementos de interface
+canvas.create_rectangle(0.0, 0.0, 670.0, 450.0, fill="#D9D9D9", outline="")  # Background direita
+canvas.create_rectangle(0.0, 0.0, 261.0, 450.0, fill="#717171", outline="")  # Background esquerda
+
+# Ícone do YouTube
+image_image_1 = PhotoImage(file=relative_to_assets("image_1.png"))
+canvas.create_image(130.0, 117.0, image=image_image_1)
+
+# Texto abaixo do ícone do YouTube
+canvas.create_text(127.0, 230.0, text="Downloader", fill="#000000", font=("Inter", 21 * -1))
+
+# Caixa de entrada de URL
+entry_image_1 = PhotoImage(file=relative_to_assets("entry_1.png"))
+canvas.create_image(130.5, 320.5, image=entry_image_1)
+entry_1 = Entry(bd=0, bg="#D9D9D9", fg="#000716", highlightthickness=0)
+entry_1.place(x=26.5, y=305.0, width=208.0, height=35.0)
+
+# Título da playlist
+canvas.create_text(292.0, 27.0, anchor="nw", text="Playlist title:", fill="#000000", font=("Inter", 21 * -1))
+text_id = canvas.create_text(410.0, 27.0, anchor="nw", text="", fill="#000000", font=("Inter", 21 * -1))
+
+# Área para vídeos baixados
+canvas.create_rectangle(292.0, 113.0, 632.0, 338.0, fill="#9F9F9F", outline="")
+canvas.create_text(292.0, 89.0, anchor="nw", text="Videos baixados:", fill="#000000", font=("Inter", 21 * -1))
+
+# Criação do Text widget para exibir mensagens de progresso
+text_widget = Text(
+    window,
+    width=47,
+    height=13.4,
+    bg="#9F9F9F",
+    fg="#000000",
+    font=("Inter", 10),
+    wrap="word",
+    bd=0,
     highlightthickness=0
 )
-entry_1.place(
-    x=26.5,
-    y=305.0,
-    width=208.0,
-    height=35.0
-)
+text_widget.insert("end", "Mensagens do terminal:\n")
+text_widget.config(state="disabled")  # Desabilita a edição inicial
 
+# Adiciona o Text widget ao Canvas
+canvas.create_window(300, 120, anchor="nw", window=text_widget)
 
-canvas.create_text(
-    292.0,
-    27.0,
-    anchor="nw",
-    text="Playlist title:",
-    fill="#000000",
-    font=("Inter", 21 * -1)
-)
-"""Caixa de texto - playlist title"""
-text_id = canvas.create_text(
-    410.0,
-    27.0,
-    anchor="nw",
-    text="",
-    fill="#000000",
-    font=("Inter", 21 * -1)
-)
-""""""
-"""Caixa de texto - videos baixados"""
-canvas.create_rectangle(
-    292.0,
-    113.0,
-    632.0,
-    338.0,
-    fill="#9F9F9F",
-    outline=""
-)
-
-# Cria o texto inicial dentro do retângulo e salva o ID do texto em `terminal_text_id`
-terminal_text_id = canvas.create_text(
-    300.0,     # Posição X
-    120.0,     # Posição Y
-    anchor="nw",
-    text="Mensagens do terminal:\n",
-    fill="#000000",
-    font=("Inter", 10),
-    width=320   # Limita a largura do texto para caber no retângulo
-)
-
-canvas.create_text(
-    292.0,
-    89.0,
-    anchor="nw",
-    text="Videos baixados:",
-    fill="#000000",
-    font=("Inter", 21 * -1)
-)
-
-button_image_1 = PhotoImage(
-    file=relative_to_assets("button_1.png"))
+# Botão de download
+button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
 button_1 = Button(
     image=button_image_1,
     borderwidth=0,
@@ -186,14 +101,9 @@ button_1 = Button(
     command=download,
     relief="flat"
 )
-button_1.place(
-    x=75.0,
-    y=359.0,
-    width=111.0,
-    height=40.0,
-    
-)
+button_1.place(x=75.0, y=359.0, width=111.0, height=40.0)
 
+# Texto informativo sobre erros de download
 canvas.create_text(
     302.0,
     347.0,
@@ -203,5 +113,8 @@ um .txt na área de trabalho""",
     fill="#000000",
     font=("Inter", 14 * -1)
 )
+
+
+# Configuração final da janela
 window.resizable(False, False)
 window.mainloop()
